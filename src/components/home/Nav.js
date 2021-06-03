dd8beb09f5f929baaee8c96d52a36e16debca7cf
evelyn9521 <evelyn9521@outlook.es>
2021-06-03 21:42:26 +0200

    proyecto

--- src/components/home/Nav.js
+++ src/components/home/Nav.js
@@ -4,13 +4,12 @@ import { NavLink } from 'react-router-dom'
 export default function Nav() {
     return (
         <div className="linksNav">
-       
-        <NavLink className="linkk" activeClassName= "text-dark" exact to="/inicio">Inicio</NavLink>
-        <NavLink className="linkk" activeClassName= "text-dark" exact to="/cuidesusalud">Cuide su salud</NavLink>
-        <NavLink className="linkk" activeClassName= "text-dark" exact to="/enlacesinteres">Enlaces de interés</NavLink>
-        <NavLink className="linkk" activeClassName= "text-dark" exact to="/chat">Chat</NavLink>
-        
-
+            <NavLink className="linkk" activeClassName= "text-dark" exact to="/inicio">Inicio </NavLink>
+            <NavLink className="linkk" activeClassName= "text-dark" exact to="/cuidesusalud">Cuide su salud</NavLink>
+            <NavLink className="linkk" activeClassName= "text-dark" exact to="/enlacesinteres">Enlaces de interés</NavLink>
+            <NavLink className="linkk" activeClassName= "text-dark" exact to="/chat">Chat</NavLink>
+            <a target="_blank" className="linkk" activeClassName= "text-dark" href="https://www.aecc.es/es/colabora/dona">Colabora</a>
+            
         </div>
     )
 }
--- src/components/home/enlacesInteres/EnlacesInteresContent.js
+++ src/components/home/enlacesInteres/EnlacesInteresContent.js
@@ -1,9 +1,33 @@
 import React from 'react'
+import m5 from "../../../image/m5.png"
 
 export default function EnlacesInteresContent() {
     return (
         <div className="content">
+            <div className="title3">
+                <h4>Aquí podrás encontrar lencería y ropa de baño para prótesis mamarias, aparte de otros productos relacionados con el cáncer de mama</h4>
+                <h5>Algunas de las webs más recomendadas son:</h5>
+            </div>
             
+            <div className="threeColumnsButtom">
+           
+                <div className="columnButtomLeft">
+                    <div><a className="letterLink" target="_blank" href="https://www.oysho.com/es/lencer%C3%ADa/sujetadores/sujetadores-post-operatorio-c1010352013.html"><button className="button5">Oysho</button></a></div>
+                    <div><a className="letterLink" target="_blank" href="https://pilarmartinezpostmastectomia.es/"><button className="button5">Pilar Martínez, Boutique</button></a></div>
+                    <div><a className="letterLink" target="_blank" href="https://www.amoena.com/es/"><button className="button5">Amoena</button></a></div>
+                </div>
+                <div>
+                    <img className="logo2" src={m5}></img>
+                </div>
+                <div className="columnButtomRight">
+                <div><a className="letterLink" target="_blank" href="https://www.boutique-cancer-de-mama.es/"><button className="button5">Boutique, Cáncer de mama</button></a></div>
+                <div><a className="letterLink" target="_blank" href="https://womensecret.com/es/es/colecciones/post-surgery"><button className="button5">Women Secret</button></a></div>
+                <div><a className="letterLink" target="_blank" href="https://www.divinaoncobeauty.com/44-banadores-mastectomia"><button className="button5">Divina Onco Beauty</button></a></div>
+               
+                </div>
+
+               
+            </div>
         </div>
     )
 }
