c0f66de7ef1c5c2e36e2c2984fb429131cd0ecb9
evelyn9521 <evelyn9521@outlook.es>
2021-06-17 13:29:29 +0200

    proyecto

--- src/components/forgetPassword/InputPassword.js
+++ src/components/forgetPassword/InputPassword.js
@@ -6,14 +6,14 @@ export default function InputPassword() {
             <div>
                 <h2 className="colorletters">Recupera tu cuenta</h2>
                 <h4 className="colorletters1">Introduce tu correo electrónico y te enviaremos un enlace para que vuelvas a entrar en tu cuenta.</h4>
-               
+
                 <div>
                     <label for="exampleInputEmail1"></label>
                     <input type="email" className="inputForgetPassword" id="exampleInputEmail1" placeholder="Correo electrónico"></input>
                 </div>
-                
+
                 <button className="button4">Enviar enlace de acceso</button>
-            
+
             </div>
         </div>
     )
